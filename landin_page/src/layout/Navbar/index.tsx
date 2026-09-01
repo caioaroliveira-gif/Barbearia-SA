1df753c565ae2901021dfe0c4219ab072fcb0b12
caioaroliveira-gif <caio_ar_oliveira@estudante.sesisenai.org.br>
2026-08-31 21:58:32 -0300

    feat: iniciando Navbar

---
+++ landin_page/src/layout/Navbar/index.tsx
@@ -0,0 +1,19 @@
+export default function Navbar() {
+  return (
+    <>
+      <header>
+        <div className=" flex mt-5 ml-5">
+          <h1>Barbearia Locals</h1>
+        </div>
+
+        <div className=" flex mt-3 justify-center ">
+          <a href="" className="m-5">Inicio</a>
+          <a href="" className="m-5">Catalogo</a>
+          <a href="" className="m-5">Sobre</a>
+        </div>
+
+
+      </header>
+    </>
+  );
+}
